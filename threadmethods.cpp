b0cb793ac67aa689ccd26bc51dbfc2f584c90b5b
hoholee12 <hoholee12@gmail.com>
2020-09-18 18:32:20 +0900

    mesa

--- threadmethods.cpp
+++ threadmethods.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[]){
     nolockjoin::test();
 
     //putandget::test();
-    putandget_twoconsumers::test();
+    //putandget_twoconsumers::test();
+    putandget_mesasemantics::test();
     return 0;
 }
